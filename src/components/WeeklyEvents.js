a1b5dba9be103d0b34f5500b266d624207915c51
lindacolumbus <lindadcolumbus@gmail.com>
2021-11-27 12:45:00 -0500

    added handlers for input change and onClick events, to push new entries to Firebase

--- src/components/WeeklyEvents.js
+++ src/components/WeeklyEvents.js
@@ -3,13 +3,13 @@ import {useState, useEffect} from 'react';
 
 function WeeklyEvents() {
     const [socialEvents, setSocialEvents] = useState([]);
+    const [userInput, setUserInput] = useState('')
     
     useEffect(() => {
         // variable that refers to database
         const dbRef = firebase.database().ref();
         // event listener to get our data from the database ('response')
         dbRef.on('value', (response) => {
-            // console.log(response.val());
             // variable to store the new state
             const newState = [];
             // store the response from Firebase inside of a variable
@@ -21,12 +21,26 @@ function WeeklyEvents() {
         })
     }, [])
 
+    // event handler for when there is a change in the add event input
+    const handleChange = (event) => {
+        setUserInput(event.target.value);
+    }
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        const dbRef = firebase.database().ref();
+        // push user input to Firebase database
+        dbRef.push(userInput);
+        // reset user input to empty string
+        setUserInput('');
+    }
+
     return (
         <section className="weekCalendar">
             {/* destructuring, to access each key-value pair within each weekday object */}
                 {socialEvents.map(({ day, eventName, eventType, partySize}) => {
                     return (
-                    <ul>
+                    <ul key={day}>
                         <li><h2>{day}</h2></li>
                         <li><p>{eventName}</p></li>
                         <li><p>{eventType}</p></li>
@@ -34,7 +48,11 @@ function WeeklyEvents() {
                     </ul>
                     )
                 })}
-            
+                <form action="submit">
+                    <label htmlFor="newEvent">Add a new event to your schedule</label>
+                    <input type="text" id="newEvent" onChange={handleChange} value={userInput}/>
+                    <button onClick={handleClick}>Add event</button>
+                </form>
         </section>
     )
 }
