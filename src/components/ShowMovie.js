b841c517ab02525ffbd8e2935811a1c2facbaa48
HugoArriojas <hugo.arriojas@hotmail.com>
2021-11-27 01:31:27 -0500

    added ShowDetails component that will show expanded information to page

--- src/components/ShowMovie.js
+++ src/components/ShowMovie.js
@@ -1,23 +1,35 @@
+// NPM Modules
+import { Link } from "react-router-dom"
 
-function ShowFilm(props) {
- 
+
+function ShowFilm({ movies }) {
+
     return (
-        <div className="movieContainer" key={props.key}>
-            <div className="image">
-                <img src={props.image} alt={`Poster of ${props.name}`} />
-            </div>
-            <div className="info">
-                <h3 className="showName">{props.name}</h3>
-                <p className="showRuntime">Runtime: {props.runtime} minutes</p>
-                <p className="showGenre">Genre: {props.genre}</p>
-                
-            </div>
-
-
-        </div>
-
+        <ul className="filmList">
+            {
+                movies.map((props) => {
+                    return (
+                        <li key={props.key}>
+                            <Link>
+                            <div className="movieContainer">
+                                <div className="image">
+                                    <img src={props.image} alt={`Poster of ${props.name}`} />
+                                </div>
+                                <div className="info">
+                                    <h3 className="showName">{props.name}</h3>
+                                    <p className="showRuntime">Runtime: {props.runtime} minutes</p>
+                                    <p className="showGenre">Genre: {props.genre}</p>
+                                </div>
+                            </div>
+                            </Link>
+                        </li>
+                    )
+                })
+            }
+        </ul>
     )
 }
+
 
 
 
