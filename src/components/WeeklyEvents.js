9b955a65a8a96b9c8e6b063b176ea63eba300095
lindacolumbus <lindadcolumbus@gmail.com>
2021-11-27 06:59:37 -0500

    added weekday header to mapped array

--- src/components/WeeklyEvents.js
+++ src/components/WeeklyEvents.js
@@ -1,10 +1,9 @@
 import firebase from '../firebase'
-import {useState, useEffect, useLayoutEffect} from 'react';
+import {useState, useEffect} from 'react';
 
 function WeeklyEvents() {
     const [socialEvents, setSocialEvents] = useState([]);
     
-
     useEffect(() => {
         // variable that refers to database
         const dbRef = firebase.database().ref();
@@ -13,26 +12,22 @@ function WeeklyEvents() {
             // console.log(response.val());
             // variable to store the new state
             const newState = [];
-            const day = [];
             // store the response from Firebase inside of a variable
             const data = response.val();
             for (let key in data) {
                 newState.push(data[key])
-                day.push(key)
-                console.log(key)
             }
-            
             setSocialEvents(newState)
         })
     }, [])
 
     return (
         <section className="weekCalendar">
-                {socialEvents.map(({ eventName, eventType, partySize}) => {
-                    console.log(socialEvents)
+            {/* destructuring, to access each key-value pair within each weekday object */}
+                {socialEvents.map(({ day, eventName, eventType, partySize}) => {
                     return (
                     <ul>
-                        {/* <li><h2>{day}</h2></li> */}
+                        <li><h2>{day}</h2></li>
                         <li><p>{eventName}</p></li>
                         <li><p>{eventType}</p></li>
                         <li><p>{partySize}</p></li>
