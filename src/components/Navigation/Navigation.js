74e927c095cb13929ad91c1c7e06b04eb5f790d5
HugoArriojas <hugo.arriojas@hotmail.com>
2021-12-02 17:34:38 -0500

    made CSS fixes with group

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import { NavItems } from "./NavigationItems";
+import { NavItems } from "./NavigationItems";
 import classes from "./Navigation.module.css";
 import Castle from "./CastleFunction";
 import "./ThemeMenu.css";
@@ -25,6 +25,17 @@ class Navigation extends Component {
         <button className={classes.themeClick} onClick={this.handleBoxToggle}>
         <i className={`${this.state.showBox ? "fas fa-bahai" : "fas fa-times"}`}></i>
         </button>
+        <ul>
+          {NavItems.map((item, index) => {
+            return (
+              <li className={classes.liststyling} href={item.url} key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.Navtitle}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </nav>
     );
   }
