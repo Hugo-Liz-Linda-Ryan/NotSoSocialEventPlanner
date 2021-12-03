53c7182b65ac6eb12e213c565c7c05d2980e2866
lizzy-bird <elizabethharris.soprano@gmail.com>
2021-12-02 22:40:19 -0500

    added border radius to forms, changed background of caledar sections, fixed calendar event text spacing

--- src/components/About/AboutDevProfilesItems.js
+++ src/components/About/AboutDevProfilesItems.js
@@ -1,45 +1,5 @@
 export const devItems = [
-    // {
-    //     devname: 'Ryan Van Den Eeckhout',
-    //     pargraph: 'Ryan Van Den Eeckhout is an aspiring Front end Web Developer further works by this developer can be found at portfilio name',
-    //     url: 'https://via.placeholder.com/150',
-    //     cName: 'appTagLine'
-
-    // },
-
-    // {
-    //     devname: 'Elizabeth Harris',
-    //     pargraph: 'Elizabeth Harris is a Front-End Developer with an extensive background in the performing arts and service industries where she has developed strong interpersonal and excellent time management skills. Elizabeth takes great pride in her ability to come up with creative solutions in the heat of the moment, and she has a passion for personal development where she continually challenges herself to learn new skills and explore new areas of interest in both her personal and professional life.',
-    //     url: 'https://via.placeholder.com/150',
-    //     cName: 'appTagLine'
-
-    // },
-
-    // {
-    //     devname: 'Hugo Arriojas',
-    //     pargraph: 'Hugo Arriojas is a front-end web developer currently located in Ottawa with a background in marine science, accounting, and statistics. Hugo Arriojas uses his strong communication and time-management skills to create an efficient and accessibility-focused work environment. Hugo is always searching for new challenges and opportunities for mentorship and growth in conjunction with his focus on the creation of positive changes in his workplace. Due to his background in marine data science and customer service roles, Hugo excels in client-facing and challenging data-driven positions where strong problem solving skills are key. Hugo has a strong understanding of web and analytical software and will use these skills to write stable, legible, and responsive code that turns ideas into reality in his professional life.',
-
-    //     url: 'https://via.placeholder.com/150',
-    //     cName: 'appTagLine'
-
-    // },
-
-    // {
-    //     devname: 'Linda Columbus',
-    //     pargraph: 'Linda Columbus is a front-end web developer with experience building clean, accessible and responsive applications with React, JavaScript (ES6+), HTML5, CSS3, and API integration. Linda brings her experience from her previous career where she managed a team, undertook the analysis of complex problems, and effectively communicated with a wide range of audiences. Linda is an empathetic collaborator who brings curiosity and enthusiasm to each team she joins and each web project she undertakes.',
-    //     url: 'https://via.placeholder.com/150',
-    //     cName: 'appTagLine'
-
-    // },
-    {
-        devname:'Hugo Arriojas',
-        paragraph: '',
-        url: '/images/1.png',
-        website: 'https://www.hugoa.dev/' ,
-        github: 'https://github.com/HugoArriojas/hugoArriojasProject03',
-        cName: 'appTagLine'
-    },    
-
+    
     {
         devname:'Linda Columbus',
         paragraph: '',
@@ -48,11 +8,18 @@ export const devItems = [
         github: 'https://github.com/lindacolumbus',
         cName: 'appTagLine'
     },
-
+    {
+        devname:'Hugo Arriojas',
+        paragraph: '',
+        url: '/images/1.png',
+        website: 'https://www.hugoa.dev/' ,
+        github: 'https://github.com/HugoArriojas/hugoArriojasProject03',
+        cName: 'appTagLine'
+    },    
     {
         devname:'Elizabeth Harris',
         paragraph: '',
-        url: '/images/3.png',
+        url: '/images/2.png',
         website: 'https://www.elizabethharris.dev' ,
         github: 'https://github.com/lizzy-bird',
         cName: 'appTagLine'
